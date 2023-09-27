fafe9122f63393d364747a39b6ea3543f215ed29
Edgar21AB <134448114+Edgar212004@users.noreply.github.com>
2023-09-27 10:50:39 -0600

    Primer commit
    Página

---
+++ Tienda_EdgarArias/src/test/java/Tienda_EdgarArias/demo/TiendaEdgarAriasApplicationTests.java
@@ -0,0 +1,13 @@
+package Tienda_EdgarArias.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaEdgarAriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
