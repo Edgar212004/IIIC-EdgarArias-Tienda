c17f9202a3d17c5dab17b07d0e1f0a27c0d49b04
Edgar21AB <134448114+Edgar212004@users.noreply.github.com>
2023-09-27 20:58:22 -0600

    Edgar
    Commit semana 3

---
+++ Tienda_EdgarA/src/main/java/Tienda_EdgarA/demo/TiendaEdgarAApplication.java
@@ -0,0 +1,13 @@
+package Tienda_EdgarA.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaEdgarAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaEdgarAApplication.class, args);
+	}
+
+}
---
+++ Tienda_EdgarA/src/test/java/Tienda_EdgarA/demo/TiendaEdgarAApplicationTests.java
@@ -0,0 +1,13 @@
+package Tienda_EdgarA.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaEdgarAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
