ef8336c9bec6f83c76fd9cfd9710d37717ea83c3
Edgar21AB <134448114+Edgar212004@users.noreply.github.com>
2023-10-11 19:25:10 -0600

    Materia vista antes de recreo

---
+++ Pagina_tarea/src/main/java/Pagina_tarea/demo/PaginaTareaApplication.java
@@ -0,0 +1,13 @@
+package Pagina_tarea.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginaTareaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginaTareaApplication.class, args);
+	}
+
+}
---
+++ Pagina_tarea/src/test/java/Pagina_tarea/demo/PaginaTareaApplicationTests.java
@@ -0,0 +1,13 @@
+package Pagina_tarea.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginaTareaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
