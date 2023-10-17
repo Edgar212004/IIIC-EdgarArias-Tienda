0450c8075b0c229b29870c8b5e705159fa6315c3
Edgar21AB <134448114+Edgar212004@users.noreply.github.com>
2023-10-16 19:14:12 -0600

    Semana 5

---
+++ TiendaEdgar/src/main/java/TiendaEdgar/TiendaEdgarApplication.java
@@ -0,0 +1,13 @@
+package TiendaEdgar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaEdgarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaEdgarApplication.class, args);
+	}
+
+}
---
+++ TiendaEdgar/src/test/java/TiendaEdgar/TiendaEdgarApplicationTests.java
@@ -0,0 +1,13 @@
+package TiendaEdgar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaEdgarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
