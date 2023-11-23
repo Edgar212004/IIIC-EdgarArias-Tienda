991a93b631f7713c77e7c96a68b33efd70d26a42
Edgar21AB <134448114+Edgar212004@users.noreply.github.com>
2023-11-23 17:16:47 -0600

    semana 11

---
+++ TiendaEdgar/src/main/java/TiendaEdgar/dao/RolDao.java
@@ -0,0 +1,8 @@
+package TiendaEdgar.dao;
+
+import TiendaEdgar.domain.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolDao extends JpaRepository<Rol, Long> {
+
+}
---
+++ TiendaEdgar/src/main/java/TiendaEdgar/service/CorreoService.java
@@ -0,0 +1,11 @@
+package TiendaEdgar.service;
+
+import jakarta.mail.MessagingException;
+
+public interface CorreoService {
+    public void enviarCorreoHtml(
+            String para, 
+            String asunto, 
+            String contenidoHtml) 
+            throws MessagingException;
+}
